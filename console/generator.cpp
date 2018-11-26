d49fbb258a050719867e0113a66c5ba4eca58ca6
lytning98 <lytning@bitnp.net>
2018-11-26 17:23:14 +0800

    Generator Finished

---
+++ console/generator.cpp
@@ -0,0 +1,54 @@
+﻿#include "stdafx.h"
+#include "generator.h"
+
+namespace generator {
+	const int space_per_cell = 4;
+	std::default_random_engine random_engine((unsigned)std::chrono::system_clock::now().time_since_epoch().count());
+
+	void forward(int* p, int step) {
+		int tmp[9];
+		for (int i = 0; i < step; i++)
+			tmp[i] = p[i];
+		for (int i = step; i < 9; i++)
+			p[i - step] = p[i];
+		for (int i = 0; i < step; i++)
+			p[9 - step + i] = tmp[i];
+	}
+
+	inline void shuffle(int* first, int* last) {
+		std::shuffle(first, last, random_engine);
+	}
+
+	void generate(FILE* file, bool empline) {
+		int map[9][9];
+		int p[9] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, q[9];
+
+		memcpy(q, p, sizeof(q));
+		std::for_each(q, q + 9, [](int& x) {x--;});
+
+		shuffle(p, p + 9);
+		for (int i = 0; i < 3; i++){
+			for (int j = 0; j < 3; j++){
+				memcpy(map[i * 3 + j], p, sizeof(p));
+				if (j == 2)
+					forward(p, 1);
+				else
+					forward(p, 3);
+			}
+		}
+		for(int i = 0; i < 3; i++){
+			for (int j = 0; j < 3; j++){
+				shuffle(q, q + 9);
+				for (int k = 0; k < 4; k++) {
+					map[i * 3 + q[k] / 3][j * 3 + q[k] % 3] = 0;
+				}
+			}
+		}
+		
+		for (int i = 0; i < 9; i++)
+			for (int j = 0; j < 9; j++)
+				fprintf(file, "%d%c", map[i][j], " \n"[j == 8]);
+		if (empline)
+			fprintf(file, "\n");
+	}
+}
---
+++ console/generator.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace generator {
+	void generate(FILE* file, bool empline);
+}
